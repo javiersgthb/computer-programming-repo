2306a90aa6bbc899260fbc6c2aa7b0b6c7f472bd
Theatre-Life <jsilva_junk@outlook.com>
2025-09-14 22:03:28 -0500

    Updating class files.

--- Assignments/Python Files/JSilva - Lesson 2 Assignment.py
+++ Assignments/Python Files/JSilva - Lesson 2 Assignment.py
@@ -6,4 +6,5 @@ age = int(current_year) - int(birth_year)
 print("Hello " + first_name + " " + last_name + "!\nYou are " + str(age) + " years old this year.")
 age += 1
 print("Next year, you will be " + str(age) + " years old.")
+
 print("Written by Javier Silva")
